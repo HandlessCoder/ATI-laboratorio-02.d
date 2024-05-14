895776baa16792a4dee96fff3f656a0387a8fdc2
Juan Pablo <www.juan201@gmail.com>
2024-05-14 18:22:36 -0400

    listado de estudiantes dinámico usando javascript

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 window.addEventListener('load',funcionInicio());
 
-
 function funcionInicio(){
+    fetch('datos/index.json').
+    then(response => response.json()).
+    then(listaPersonas => {    
+        
+        listaPersonas.forEach(persona => {
+            console.log(persona.nombre+" "+persona.ci+"\n");
+            let contenedor = document.getElementById('whiteboard');
+            
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('profile');
+            newDiv.innerHTML = `<img src="${persona.imagen}" class="image" title=""> <br> <span>${persona.nombre}</span>`;
 
+            contenedor.appendChild(newDiv);
+        });
+    })
 }
