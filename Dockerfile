1748b050f84e25003212d1cdb17c1c464455c0d0
Juan Pablo <www.juan201@gmail.com>
2024-06-15 11:51:27 -0400

    multipage to singlepage, inner comments and dockerfile change

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM ubuntu:latest
-
 RUN apt-get update && apt-get install -y apache2
-
 COPY . /var/www/html/
-
 EXPOSE 80
 
 CMD ["/usr/sbin/apache2ctl", "-D", "FOREGROUND"]
+
+
+
+#usar "docker build -t reto09-ati ." en el terminal del SO
+#luego "docker run -tid --name container-reto09-ati -p 8080:80 reto09-ati"
+#por último, visitar el URL localhost:8080, pudiendo ademas añadir parámetro para el idioma, con ?lan=IDIOMA (PT, EN, ES)
--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
-
-
 window.addEventListener('load', () => {
-    funcionInicio();
-    window.addEventListener('load',showProfile())
-    document.getElementById("Buscar").addEventListener("click",
+    funcionInicio();                                //esta es la funcion para cargar dinámicamente el contenido del mainpage
+    document.getElementById("Buscar").addEventListener("click",     //event listener de la búsqueda en mainpage, al hacer click
         () => buscar(document.getElementById("text").value));
                 
-        document.getElementById("text").addEventListener('keydown',
+        document.getElementById("text").addEventListener('keydown', //event listener de la búsqueda en mainpage, al presionar enter
         (e)  => {  if (e.key ==="Enter")   buscar(document.getElementById("text").value); 
     });
 });
 
 function funcionInicio(){
-    if( document.title == "ATI[UCV] 2024-1"
-    // window.location.pathname === "/index.html" ||  window.location.pathname === "/"
-){
-        let parametros = window.location.search;
-        let searchParams = new URLSearchParams(parametros);
-        let idioma = searchParams.get('lan');
+    if( document.title == "ATI[UCV] 2024-1" //se verifica que el título de la página corresponda con la página principal
+){      
+        document.getElementById('profile-body').style.display = 'none'      //oculta el div de datos del perfil
+        document.getElementById('mainpage-body').style.display = 'block'    //muestra el div de la página principal
         
-        if(!(idioma==='EN'||idioma==='PT'||idioma==='ES'))  idioma = 'ES'
+        let parametros = window.location.search;                            //se obtienen los parámetros pasados por URL
+        let searchParams = new URLSearchParams(parametros);                 //se convierte en objeto iterable
+        let idioma = searchParams.get('lan');                               //se escoge el parámetro bajo el key "lan" para el idioma
+        
+        if(!(idioma==='EN'||idioma==='PT'||idioma==='ES'))  idioma = 'ES'   //si lo obtenido es distinto de ES, EN o PT, entonces será ES por defecto
 
-        fetch(`conf/config${idioma}.json`).
+        
+        fetch(`conf/config${idioma}.json`).                                 //en funcion del idioma se cargan todos los textos de la página
         then(response => response.json()).
         then(listaTextos => {
             document.getElementById('nav1').innerHTML=`${listaTextos.sitio[0]} <sub>${listaTextos.sitio[1]}</sub>&nbsp;${listaTextos.sitio[2]}`
@@ -29,7 +29,7 @@ function funcionInicio(){
             fetch('datos/index.json').
             then(response => response.json()).
             then(listaPersonas => {    
-                document.getElementById('nav2').innerHTML=`${listaTextos.saludo},&nbsp;<a href="./perfil.html?ci=${listaPersonas[0].ci}&lan=${idioma}"> ${listaPersonas[0].nombre} </a>`
+                document.getElementById('nav2').innerHTML=`${listaTextos.saludo},&nbsp;<span id="main-user" onclick=" document.title = 'Perfil:';showProfile(${listaPersonas[0].ci})"> ${listaPersonas[0].nombre} </span>`
             })
             document.getElementById('text').setAttribute('placeholder',`${listaTextos.nombre}...`)
             document.getElementById('Buscar').innerHTML = `${listaTextos.buscar}...`
@@ -37,26 +37,23 @@ function funcionInicio(){
         })
 
         fetch('datos/index.json').
-        then(response => response.json()).
+        then(response => response.json()).                      //se cargan todos los datos de las personas almacenadas en el JSON file consultado
         then(listaPersonas => {    
             
             listaPersonas.forEach(persona => {
-                // console.log(persona.nombre+" "+persona.ci+"\n");
-                let contenedor = document.getElementById('whiteboard');
-                contenedor.innerHTML+=  `<a href = "perfil.html?ci=${persona.ci}&lan=${idioma}"><div class="profile"> <img src="directorio\/${persona.imagen}" class="image" title=""> <br> <span>${persona.nombre}</span></div></a>`
-
+                SumarInnerPorId('whiteboard',`<div class="profile" onclick=" document.title = 'Perfil:';showProfile(${persona.ci})"> <img src="directorio\/${persona.imagen}" class="image" title=""> <br> <span>${persona.nombre}</span></div>`)
             });
         })
-    }//else console.log('No estoy en inicio')
+    }
 }
 
-function SumarInnerPorId(id,valor,antes = false){
+function SumarInnerPorId(id,valor,antes = false){   //esta funcion se usa para facilitar la adicion de contenido en el innerHTML de las etiquetas del documento en cuestión
     if(typeof valor === 'object'){
         if(valor.length == 1)
             valor = valor[0]
         else {
             let string = valor [0];
-            let length = valor.length
+            let length = valor.length       //si el value pasado es una lista, entonces esta se desarma para crear un string con todos los valores internos de la lista, usando comas e "y"
             for(let i = 1; i<valor.length;i++){
                 if(i<length-1)
                     string+=`, ${valor[i]}`
@@ -66,16 +63,15 @@ function SumarInnerPorId(id,valor,antes = false){
             valor = string
         }
     }   
-    if(!antes)   document.getElementById(id).innerHTML+=valor;
+    if(!antes)   document.getElementById(id).innerHTML+=valor;  //si el valor va después, se usa +=, si no entonces el valor deberá ir antes de lo que había anteriormente en el innerHTML
     else    document.getElementById(id).innerHTML = valor + document.getElementById(id).innerHTML;
 }
 
-function buscar(nombre){
-    let fondo = document.getElementById('whiteboard');
-    fondo.innerHTML="";
+function buscar(nombre){                            //funcion encargada de realizar la búsqueda de personas en el mainpage
+    document.getElementById('whiteboard').innerHTML="";     //primero vacía el espacio de la muestra
 
     let parametros = window.location.search;
-    let searchParams = new URLSearchParams(parametros);
+    let searchParams = new URLSearchParams(parametros);     //se hace el mismo trabajo de antes para obtener el idioma
     let idioma = searchParams.get('lan');
     
     if(!(idioma==='EN'||idioma==='PT'||idioma==='ES'))  idioma = 'ES'
@@ -85,61 +81,60 @@ function buscar(nombre){
     then(response => response.json()).
     then(listaPersonas => {    
         listaPersonas.forEach(persona => {
-            let expReg = new RegExp(nombre,"i");
+            let expReg = new RegExp(nombre,"i");    //se compara el nombre de cada persona contra la regExp generada por la búsqueda
             if(persona.nombre.match(expReg)){
-                hayResultado = true;
+                hayResultado = true;            //si se encuentra por lo menos un resultado, entonces se altera el valor de esta variable y se agrega donde corresponde
                 console.log(hayResultado)
-
-                fondo.innerHTML+=  `<a href = "perfil.html?ci=${persona.ci}&lan=${idioma}"><div class="profile"> <img src="directorio\/${persona.imagen}" class="image" title=""> <br> <span>${persona.nombre}</span></div></a>`
+                SumarInnerPorId('whiteboard',`<div class="profile" onclick=" document.title = 'Perfil:';showProfile(${persona.ci})"> <img src="directorio\/${persona.imagen}" class="image" title=""> <br> <span>${persona.nombre}</span></div>`)
             }});
             if(!hayResultado){
                 console.log(hayResultado)
-                fetch(`conf/config${idioma}.json`).
+                fetch(`conf/config${idioma}.json`).     //si no se encuentra nada, entonces se usa la configuración de idioma para mostrar un mensaje que reporte la inexistencia de algún nombre que coincida con la regExp ingresada.
                 then(response => response.json()).
                 then(listaTextos => {
                     textos = listaTextos.not_found.split("[query]");
-                    fondo.innerHTML+=`<span id="noMatch"> ${textos[0]} "${document.getElementById("text").value}" ${textos[1]} </span>`
+                    SumarInnerPorId('whiteboard',`<span id="noMatch"> ${textos[0]} "${document.getElementById("text").value}" ${textos[1]} </span>`)
                 })
             }
     })
 }
 
-function showProfile(){
+function showProfile(cedula){                   //funcion usada para que se muestre el apartado de perfil en la web
     if(
-        document.title.match(new RegExp("Perfil:"))
-        // window.location.pathname==="/perfil.html"
+        document.title.match(new RegExp("Perfil:"))             //se comprueba que se está en el estado correcto para poder ejecutar las instrucciones siguientes.
     ){
+        document.getElementById('profile-body').style.display = 'block'         //se muestra el contenedor del perfil
+        document.getElementById('mainpage-body').style.display = 'none'         //se oculta el contenedor del mainpage
         let parametros = window.location.search;
         let searchParams = new URLSearchParams(parametros);
         let idioma = searchParams.get('lan');
         
-        if(!(idioma==='EN'||idioma==='PT'||idioma==='ES'))  idioma = 'ES'
+        if(!(idioma==='EN'||idioma==='PT'||idioma==='ES'))  idioma = 'ES'       //mismo conjunto de instrucciones para obtener el idioma.
 
         fetch(`conf/config${idioma}.json`).
         then(response => response.json()).
         then(listaTextos => {
             SumarInnerPorId('color-pregunta',listaTextos.color)
-            SumarInnerPorId('libro-pregunta',listaTextos.libro)
+            SumarInnerPorId('libro-pregunta',listaTextos.libro)         //se usan los textos de cada idioma para ir llenando el arbol DOM del perfil
             SumarInnerPorId('musica-pregunta',listaTextos.musica)
             SumarInnerPorId('juegos-pregunta',listaTextos.video_juego)
             SumarInnerPorId('lenguajes-pregunta',listaTextos.lenguajes)
             
-            let cedula = searchParams.get('ci');
             fetch(`directorio/${cedula}/perfil.json`).
-            then(response => response.json()).
+            then(response => response.json()).              //se imprime el nombre de la persona
             then( listaAtributos => {
                 document.getElementById('nombre').innerHTML= listaAtributos.nombre;
                 
                 fetch('datos/index.json').
                 then(response => response.json()).
-                then(listaPersonas => {    
-                    let personaActual = listaPersonas.find(persona => persona.ci == cedula);
+                then(listaPersonas => {                     //se muestra la foto de la persona en la página
+                    let personaActual = listaPersonas.find(persona => persona.ci == cedula);   
                     document.title = `Perfil: ${personaActual.nombre}`;
                     document.getElementById('mi-foto').setAttribute("src","directorio\/"+personaActual.imagen);
                 })
                 SumarInnerPorId('descripcion',listaAtributos.descripcion)
                 SumarInnerPorId('color-respuesta',listaAtributos.color)
-                SumarInnerPorId('libro-respuesta',listaAtributos.libro)
+                SumarInnerPorId('libro-respuesta',listaAtributos.libro) //se concatenan las respuestas de cada persona en cada área
                 SumarInnerPorId('musica-respuesta',listaAtributos.musica)
                 SumarInnerPorId('juegos-respuesta',listaAtributos.video_juego)
                 SumarInnerPorId('lenguajes-respuesta',listaAtributos.lenguajes)
@@ -147,7 +142,10 @@ function showProfile(){
                 let emailStrings = listaTextos.email.split("[email]");
                 let contenidoEmail = `${emailStrings[0]} <a href=mailto:${listaAtributos.email} id=mail> <span id="correo-respuesta"> ${listaAtributos.email}</span></a> ${emailStrings[1]}`
                 SumarInnerPorId('correo-pregunta',contenidoEmail)
+
+                //se añade el mensaje (dependiente del idioma) de que en caso de necesitar contactarlo, puede hacerlo a la dirección dada. También se añade el enlace mailto: a la dirección de correo
             })
         })
-    }//else console.log("No estoy en perfil")
+        document.getElementById('atras-anchor').setAttribute('href',`index.html?lan=${idioma}`)
+    }
 }
